bdf5c58b1ce0a59db52e55c1a9629cead71d201b
Daniël Pross <daniel@pross.nl>
2022-04-09 16:30:28 +0200

    subfolders op hoger level

--- photosort.py
+++ photosort.py
@@ -2,13 +2,13 @@
 import shutil
 from os import listdir,path,makedirs
 import datetime
-import PySimpleGUI as sg
 import exifread
 from joblib import Parallel, delayed
 import multiprocessing
 
-source = "E:/DCIM/100MSDCF/"
-dest = "C:/Users/daniel/Pictures/import/"
+# source = "E:/DCIM/100MSDCF/"
+source = "C:/Users/danie/Pictures/tmp/"
+dest = "C:/Users/danie/Pictures/dest/"
 filelist = listdir(source)
 
 num_cores = multiprocessing.cpu_count()
@@ -22,18 +22,11 @@ def copyphotos(source, dest, x):
   if creation_time != None:
     date = datetime.datetime.strptime(str(creation_time),"%Y:%m:%d %H:%M:%S")
     extension = path.splitext(source+x)[1]
-    destpath = dest+str(date.year)+"/"+str(date.month)+"/"+str(date.day)+"/"
+    destpath = dest+str(extension[1:])+"/"+str(date.year)+"/"+str(date.month)+"/"+str(date.day)+"/"
+    if not path.exists(destpath+x):
+      makedirs(path.dirname(destpath), exist_ok=True)
+    shutil.copy(source+x, destpath+x )
+    print("copied file "+source+x+" to "+destpath+x)
 
-    if extension == ".JPG":
-      if not path.exists(destpath+x):
-        makedirs(path.dirname(destpath), exist_ok=True)
-      shutil.copy(source+x, destpath+x )
-      print("copied file "+source+x+" to "+destpath+x)
-    else:
-      destpath = str(destpath+extension[1:]+"/")
-      if not path.exists(destpath+x):
-        makedirs(path.dirname(destpath), exist_ok=True)
-      shutil.copy(source+x, destpath+x )
-      print("copied file "+source+x+" to "+destpath+x)
 
 Parallel(n_jobs=num_cores)(delayed(copyphotos)(source,dest,x)for x in filelist)
